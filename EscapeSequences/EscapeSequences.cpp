f41cb6742e62fe53b6449dd29839fd77fcec6445
Abdulrahman Fawzi <120297251+AbdulrahmanFawzi@users.noreply.github.com>
2026-01-19 13:23:01 +0300

    Practice EsxapeSequences

---
+++ EscapeSequences/EscapeSequences.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+
+int main (){
+
+    cout << "\a\n";
+
+    cout << "dear sir\\Abdo, \n";
+    cout<< "how are you?\n";
+    cout<<"My name is \"Abdulrahman\" \n";
+
+
+    cout<< "Ali\t Fawzi";
+
+    return 0 ; 
+}
